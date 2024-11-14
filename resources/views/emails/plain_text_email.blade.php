106b594a834e13d770c6578518e5bd9850d9646e
Cyril Izuchukwu <alexcyril34@gmail.com>
2024-11-14 10:42:39 +0100

    Adjusted the email template

--- resources/views/emails/plain_text_email.blade.php
+++ resources/views/emails/plain_text_email.blade.php
@@ -33,7 +33,10 @@
                     <tr>
                         <td style="background-color: #fff;">
                             <div style="position: relative; width: 100%; height: 140px; border-radius: 10px;">
-                                <img src="{{ asset('xhtml/images/pattern/1.png') }}" alt="Main Image" style="position: relative; width: 100%; height: 100%; border-radius: 10px; object-fit:cover;  object-position: center;">
+                                <!-- <img src="{{ asset('xhtml/images/pattern/1.png') }}" alt="Main Image" style="position: relative; width: 100%; height: 100%; border-radius: 10px; object-fit:cover;  object-position: center;"> -->
+
+                                <img src="{{ config('app.url') . '/xhtml/images/pattern/1.png' }}" alt="Main Image" style="position: relative; width: 100%; height: 100%; border-radius: 10px; object-fit:cover; object-position: center;">
+
                             </div>
                         </td>
                     </tr>
